d2c7fa5828d45b4f806f8c9fe9f4ee78948f4639
anomalou254 <anomalou254@gmail.com>
2023-05-28 14:42:20 +0300

    testing hosting44

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -58,9 +58,21 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'portfolio.urls'
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-}
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
