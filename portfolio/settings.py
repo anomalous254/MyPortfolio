0d597f37e1fc6f73897dd60c071e9b5d76d63c12
anomalou254 <anomalou254@gmail.com>
2023-05-28 22:49:36 +0300

    corrected grammer error

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,3 +132,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "NyandoLabs | Admin",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "NyandoLabs",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "NyandoLabs",
+    }
