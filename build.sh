210e402eaed612cc23c88b1749e8ddbb907fed67
anomalou254 <anomalou254@gmail.com>
2023-05-28 14:18:38 +0300

    testing hosting44

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit  # exit on error
 
 pip install -r requirements.txt
-python3.9 -m pip install --upgrade pip
+pip install --upgrade pip
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
