8d2dc037c2d540d6cb4a96c646300eddb1bc6f3e
anomalou254 <anomalou254@gmail.com>
2023-05-28 22:56:28 +0300

    updated the admin logging greet text

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,4 +142,5 @@ JAZZMIN_SETTINGS = {
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "NyandoLabs",
+    "welcome_sign": "Welcome to NyandoLabs",
     }
